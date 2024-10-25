6435fe79d82551180e1c1d9953ac78551c1f3c06
Erlend Johnsen <erea@ihelse.net>
2024-10-25 15:34:53 +0200

    Switched to using new tabs in homeview

--- Hviktor-web/src/app/_components/home/home.component.ts
+++ Hviktor-web/src/app/_components/home/home.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { TabsModule } from 'primeng/tabs';
+import { NewsFeedComponent } from '../news-feed/news-feed.component';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
+  imports: [TabsModule, CardModule, NewsFeedComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
--- Hviktor-web/src/app/_components/news-feed/news-feed.component.ts
+++ Hviktor-web/src/app/_components/news-feed/news-feed.component.ts
@@ -1,7 +1,12 @@
 import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { CheckboxModule } from 'primeng/checkbox';
+import { ChipModule } from 'primeng/chip';
 
 @Component({
   selector: 'app-news-feed',
+  standalone: true,
+  imports: [CardModule, CheckboxModule, ChipModule],
   templateUrl: './news-feed.component.html',
   styleUrl: './news-feed.component.scss'
 })
