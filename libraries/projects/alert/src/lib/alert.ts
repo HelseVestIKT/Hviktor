3e798be4e66573a6fecc1c4a8fffd25da24098ca
Erlend Johnsen <erea@ihelse.net>
2026-01-13 10:38:38 +0100

    adds libraries and projects

---
+++ libraries/projects/alert/src/lib/alert.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-alert',
+  imports: [],
+  template: `
+    <p>
+      alert works!
+    </p>
+  `,
+  styles: ``,
+})
+export class Alert {
+
+}
