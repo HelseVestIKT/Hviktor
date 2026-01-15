efe4dee7b4be54a875b8fb243b1d7c744490e3cd
Erlend Johnsen <erea@ihelse.net>
2026-01-15 12:05:27 +0100

    adds forms folder

--- hviktor-angular/projects/hviktor/src/forms/field/index.ts
+++ hviktor-angular/projects/hviktor/src/forms/field/index.ts
@@ -1,5 +1,4 @@
 export { HviField } from './field.component';
-export { HviFieldCounter } from './field-counter.directive';
 export { HviFieldDescription } from './field-description.directive';
 export { HviFieldOptional } from './field-optional.directive';
 export { HviFieldValidation } from './field-validation.directive';
--- hviktor-angular/src/app/app.ts
+++ hviktor-angular/src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HviAvatar, HviAlert, HviButton, HviHeading, HviParagraph, HviCard, HviCardBlock, HviLabel, HviBreadcrumbs, HviFieldset, HviField, HviFieldDescription, HviFieldCounter, HviFieldOptional, HviFieldValidation  } from '@helsevestikt/hviktor';
+import { HviAvatar, HviAlert, HviButton, HviHeading, HviParagraph, HviCard, HviCardBlock, HviLabel, HviBreadcrumbs, HviFieldset, HviField, HviFieldDescription, HviFieldOptional, HviFieldValidation  } from '@helsevestikt/hviktor';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HviButton, HviAlert, HviAvatar, HviHeading, HviParagraph, HviCard, HviCardBlock, HviLabel, HviBreadcrumbs, HviFieldset, HviField, HviFieldDescription, HviFieldOptional, HviFieldValidation, HviFieldCounter],
+  imports: [RouterOutlet, HviButton, HviAlert, HviAvatar, HviHeading, HviParagraph, HviCard, HviCardBlock, HviLabel, HviBreadcrumbs, HviFieldset, HviField, HviFieldDescription, HviFieldOptional, HviFieldValidation],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
