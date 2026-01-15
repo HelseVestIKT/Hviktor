0a55d72a6a2b215b4603324f51cc85a5c696ce0c
Erlend Johnsen <erea@ihelse.net>
2026-01-15 13:22:23 +0100

    adds docs on field affix

--- hviktor-angular/projects/hviktor/src/forms/field/field-affix.component.ts
+++ hviktor-angular/projects/hviktor/src/forms/field/field-affix.component.ts
@@ -1,5 +1,21 @@
-import { Component, Input } from "@angular/core";
+import { Component } from "@angular/core";
 
+/**
+ * Decorative affix container displayed alongside a text input.
+ *
+ * @remarks
+ * Used together with the `hvi-field-affixes` component to wrap leading and trailing adornments.
+ * 
+ * @example
+ * ```html
+ *  <hvi-field-affixes>
+ *    <hvi-field-affix>NOK</hvi-field-affix>
+ *    <input hviInput type="text" placeholder="Beløp" />
+ *    <hvi-field-affix>pr. mnd.</hvi-field-affix>
+ * </hvi-field-affixes>
+ * ```
+ * 
+ */
 @Component({
     selector: "hvi-field-affix",
     template: `<ng-content />`,
@@ -8,5 +24,4 @@ import { Component, Input } from "@angular/core";
         '[aria-hidden]': "true",
     },
 })
-export class HviFieldAffix {
-}
+export class HviFieldAffix {}
--- hviktor-angular/projects/hviktor/src/forms/field/field-affixes.component.ts
+++ hviktor-angular/projects/hviktor/src/forms/field/field-affixes.component.ts
@@ -1,5 +1,21 @@
 import { Component } from "@angular/core";
 
+/**
+* Container for decorative affixes displayed alongside a text input.
+* 
+* @remarks
+* Wraps leading and trailing adornments provided by `hvi-field-affix` components.
+* 
+* @example
+* ```html
+*   <hvi-field-affixes>
+*       <hvi-field-affix>NOK</hvi-field-affix>
+*       <input hviInput type="text" placeholder="Beløp" />
+*       <hvi-field-affix>pr. mnd.</hvi-field-affix>
+* </hvi-field-affixes>
+* ```
+* 
+*/
 @Component({
     selector: "hvi-field-affixes",
     template: `<ng-content />`,
