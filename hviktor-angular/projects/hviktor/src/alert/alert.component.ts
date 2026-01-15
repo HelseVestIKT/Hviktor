b679908d75dbd32ebe4c3da5986c5e18f8b2de0f
Erlend Johnsen <erea@ihelse.net>
2026-01-15 13:27:25 +0100

    adds docs on alert

--- hviktor-angular/projects/hviktor/src/alert/alert.component.ts
+++ hviktor-angular/projects/hviktor/src/alert/alert.component.ts
@@ -1,5 +1,18 @@
-import { Component, HostBinding, Input } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
+/**
+ * Alert gir brukeren informasjon som det er ekstra viktig at de ser og forstår. 
+ * Komponenten er designet for å fange brukernes oppmerksomhet. 
+ * Teksten i varselet skal være kort og tydelig.
+ * 
+ * Eksempel på bruk:
+ * ```html
+ * <hvi-alert color="warning">
+ *  Dette er et advarselsvarsel!
+ * </hvi-alert>
+ * ```
+ * 
+ */
 @Component({
   selector: 'hvi-alert',
   standalone: true,
@@ -7,10 +20,9 @@ import { Component, HostBinding, Input } from "@angular/core";
   host: {
     class: 'ds-alert',
     '[attr.data-color]': 'color'
-
   },
 })
 export class HviAlert {
-  /** The color theme of the alert */
-    @Input() color: 'info' | 'success' | 'warning' | 'danger' = 'info';
+  /** Setter typen varsel ved å endre farge og ikon */
+    @Input() color?: 'info' | 'success' | 'warning' | 'danger';
 }
