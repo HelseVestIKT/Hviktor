5aaf61444da37725980b80b7f605dd1a1649c460
Erlend Johnsen <erea@ihelse.net>
2026-01-16 14:45:03 +0100

    adds dialog-block

--- hviktor-angular/src/app/app.ts
+++ hviktor-angular/src/app/app.ts
@@ -1,11 +1,66 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HviAvatar, HviAlert, HviButton, HviHeading, HviParagraph, HviDivider, HviCard, HviDetails, HviDetailsSummary, HviDetailsContent, HviCardBlock, HviLabel, HviBreadcrumbs, HviBadgePosition, HviBadge, HviFieldset, HviField, HviFieldDescription, HviFieldOptional, HviFieldAffix, HviFieldAffixes, HviInput, HviChipLabel, HviChipButton, HviDialog  } from '@helsevestikt/hviktor';
+import { 
+  HviAvatar, 
+  HviAlert, 
+  HviButton, 
+  HviHeading, 
+  HviParagraph, 
+  HviDivider, 
+  HviCard, 
+  HviDetails, 
+  HviDetailsSummary, 
+  HviDetailsContent, 
+  HviCardBlock, 
+  HviLabel, 
+  HviBreadcrumbs, 
+  HviBadgePosition, 
+  HviBadge, 
+  HviFieldset, 
+  HviField, 
+  HviFieldDescription, 
+  HviFieldOptional, 
+  HviFieldAffix, 
+  HviFieldAffixes, 
+  HviInput, 
+  HviChipLabel, 
+  HviChipButton, 
+  HviDialog, 
+  HviDialogBlock  
+} from '@helsevestikt/hviktor'; 
 import '@u-elements/u-details';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HviButton, HviAlert, HviAvatar, HviBadge, HviDetails, HviDivider, HviDetailsSummary, HviDetailsContent, HviHeading, HviParagraph, HviCard, HviCardBlock, HviLabel, HviBreadcrumbs, HviBadgePosition, HviFieldset, HviField, HviFieldDescription, HviFieldOptional, HviFieldAffix, HviFieldAffixes, HviInput, HviChipLabel, HviChipButton, HviDialog],
+  imports: [
+    RouterOutlet, 
+    HviButton, 
+    HviAlert, 
+    HviAvatar, 
+    HviBadge, 
+    HviDetails, 
+    HviDivider, 
+    HviDetailsSummary, 
+    HviDetailsContent, 
+    HviHeading, 
+    HviParagraph, 
+    HviCard, 
+    HviCardBlock, 
+    HviLabel, 
+    HviBreadcrumbs, 
+    HviBadgePosition, 
+    HviFieldset, 
+    HviField, 
+    HviFieldDescription, 
+    HviFieldOptional, 
+    HviFieldAffix, 
+    HviFieldAffixes, 
+    HviInput, 
+    HviChipLabel, 
+    HviChipButton, 
+    HviDialog, 
+    HviDialogBlock,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
