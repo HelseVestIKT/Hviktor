4e488c84d1f212e763f5390803301afb1734234e
Erlend Johnsen <erea@ihelse.net>
2026-02-05 15:08:03 +0100

    finished forms demo

--- hviktor-angular/src/app/app.ts
+++ hviktor-angular/src/app/app.ts
@@ -18,13 +18,13 @@ import {
   HviDialog,
   HviDialogBlock,
   HviDivider,
-  HviErrorSummary,
   HviFieldKit,
   HviForms,
   HviHeading,
   HviLabel,
   HviParagraph,
   HviValidationKit,
+  HviValidationMessages,
 } from '@helsevestikt/hviktor';
 import '@u-elements/u-details';
 
@@ -51,7 +51,6 @@ import '@u-elements/u-details';
     HviChipButton,
     HviDialog,
     HviDialogBlock,
-    HviErrorSummary,
     HviForms,
     HviFieldKit,
     HviValidationKit,
@@ -72,12 +71,59 @@ export class App {
   }
 
   form = new FormGroup({
+    // Person
     firstName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    lastName: new FormControl('', [Validators.required, Validators.minLength(2)]),
+
+    // Kontakt
+    email: new FormControl('', [Validators.required, Validators.email]),
     phone: new FormControl('', [Validators.required, Validators.pattern(/^\d+$/)]),
+
+    // Økonomi
+    monthlyCost: new FormControl<number | null>(null, [Validators.required, Validators.min(0)]),
+
+    // Preferanser
+    gender: new FormControl('', [Validators.required]),
+    flightMode: new FormControl(false),
+
+    // Samtykke
+    consent: new FormControl(false, [Validators.requiredTrue]),
   });
 
-  messages = {
-    firstName: { required: 'Fornavn er påkrevd', minlength: 'Fornavn må være minst 2 tegn' },
-    phone: { required: 'Telefon er påkrevd', pattern: 'Telefonnummer kan kun inneholde siffer' },
-  } as const;
+  messages: Record<string, HviValidationMessages> = {
+    firstName: {
+      required: 'Fornavn er påkrevd',
+      minlength: 'Fornavn må være minst 2 tegn',
+    },
+    lastName: {
+      required: 'Etternavn er påkrevd',
+      minlength: 'Etternavn må være minst 2 tegn',
+    },
+    email: {
+      required: 'E-post er påkrevd',
+      email: 'E-post må være gyldig',
+    },
+    phone: {
+      required: 'Telefon er påkrevd',
+      pattern: 'Telefonnummer kan kun inneholde siffer',
+    },
+    monthlyCost: {
+      required: 'Beløp er påkrevd',
+      min: 'Beløp kan ikke være negativt',
+    },
+    gender: {
+      required: 'Velg et alternativ',
+    },
+    consent: {
+      requiredTrue: 'Du må samtykke før du kan sende inn',
+    },
+  };
+
+  onSubmit(): void {
+    if (this.form.valid) {
+      alert('Skjema sendt inn!');
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
 }
