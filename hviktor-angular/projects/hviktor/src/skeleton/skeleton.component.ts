36437a3b49dde2caa2ffcdb5edba9a46e0f6ff7f
Erlend Johnsen <erea@ihelse.net>
2026-02-12 15:26:48 +0100

    creates skeleton component

---
+++ hviktor-angular/projects/hviktor/src/skeleton/skeleton.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+/**
+ * @summary
+ * Skeleton component description.
+ *
+ * @example
+ * ```html
+ * <hvi-skeleton></hvi-skeleton>
+ * ```
+ *
+ * @see {@link https://designsystemet.no/en/components/docs/skeleton/code}
+ */
+
+@Component({
+  selector: 'hvi-skeleton',
+  standalone: true,
+  template: '<ng-content />',
+  host: {},
+})
+export class HviSkeleton {}
