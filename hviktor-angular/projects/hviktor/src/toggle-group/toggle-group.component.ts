d728fcbf087f9029f7b906daf71b7c491265461c
Erlend Johnsen <erea@ihelse.net>
2026-02-13 12:57:55 +0100

    adds togglegroup scaffold

---
+++ hviktor-angular/projects/hviktor/src/toggle-group/toggle-group.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+/**
+ * @summary
+ * ToggleGroup component description.
+ *
+ * @example
+ * ```html
+ * <hvi-toggle-group></hvi-toggle-group>
+ * ```
+ *
+ * @see {@link https://designsystemet.no/en/components/docs/toggle-group/code}
+ */
+
+@Component({
+  selector: 'hvi-toggle-group',
+  standalone: true,
+  template: '<ng-content />',
+  host: {},
+})
+export class HviToggleGroup {}
