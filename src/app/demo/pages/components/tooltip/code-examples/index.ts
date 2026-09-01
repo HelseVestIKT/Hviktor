fee0a0c6cb0d249a5cfa974b870ae60fccea5fc1
Erlend Johnsen <erea@ihelse.net>
2026-09-01 13:18:03 +0200

    feat(tooltip): add new tooltip examples and remove deprecated ones

--- src/app/demo/pages/components/tooltip/code-examples/index.ts
+++ src/app/demo/pages/components/tooltip/code-examples/index.ts
@@ -1,7 +1,9 @@
 // Auto-generated - do not edit manually
+export * from './tooltip.betinget-tooltip.example';
+export * from './tooltip.betinget-tooltip.example.source';
 export * from './tooltip.ikon-knapp-med-tooltip.example';
 export * from './tooltip.ikon-knapp-med-tooltip.example.source';
+export * from './tooltip.med-tekst.example';
+export * from './tooltip.med-tekst.example.source';
 export * from './tooltip.plassering.example';
 export * from './tooltip.plassering.example.source';
-export * from './tooltip.tekst-med-tooltip.example';
-export * from './tooltip.tekst-med-tooltip.example.source';
