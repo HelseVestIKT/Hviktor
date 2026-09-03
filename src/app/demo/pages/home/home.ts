b4b46bfe58548930adbbebf1707cd5464400fff6
Erlend Johnsen <erea@ihelse.net>
2026-09-03 14:11:29 +0200

    refactor(home): remove unused button from home component template

--- src/app/demo/pages/home/home.ts
+++ src/app/demo/pages/home/home.ts
@@ -16,7 +16,6 @@ import { DEMO_COMPONENTS } from '../../demo-components';
   template: `
     <header class="mb-8 max-w-xl">
       <h1 hviHeading size="xl">Hviktor Angular Demo</h1>
-      <button>Klikk meg</button>
       <p hviParagraph>
         Demo av komponenter fra
         <a
